050e0cd8760de12b4f1be8734a943b3a66c91426
동섭 <DONGSEUP@dongseobs-MacBookPro.local>
2023-05-30 21:23:47 +0900

    ADD : 주문내역 페이지 스타일 완성

---
+++ src/pages/Order/Order.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import classes from './order.module.scss';
+
+const Order = () => {
+  const navigate = useNavigate();
+  return (
+    <ul className={classes.wrapOrder}>
+      <li className={classes.orderItem}>
+        <div className={classes.top}>
+          <div className={classes.date}>4/20 (목)</div>
+          <div className={classes.state}>• 배달완료</div>
+        </div>
+        <div className={classes.mid}>
+          <img src="/image/brandLogo/bbqLogo.png" alt="store_img" />
+          <div className={classes.info}>
+            <div
+              className={classes.name}
+              onClick={() => navigate(`/storeDetail/id`)}
+            >
+              가계이름 {'>'}
+            </div>
+            <div className={classes.orderInfo}>
+              <div className={classes.menu}>주문음식 이름</div>
+              {/* 2개 이상이면 외? 몇개로 표시 */}
+              <div className={classes.quantity}>?개</div>
+              {/* 총금액 */}
+              <div className={classes.price}>30,000원</div>
+            </div>
+          </div>
+        </div>
+        <button>리뷰 쓰기</button>
+      </li>
+    </ul>
+  );
+};
+
+export default Order;
